cbb83ee000e34b56583bb99865992620259fb92a
jgiboney <jgiboney@gmail.com>
2017-05-09 09:29:51 -0400

    Update cross.plot.R

--- cross.plot.R
+++ cross.plot.R
@@ -1,6 +1,5 @@
 cross.plot <- function (data, x, y, cross, x.lab, y.lab, cross.lab, theme, 
                         legend.pos, pos.dodge, output.path, output.h, output.w) {
-  pdf(output.path, height=output.h, width=output.w)
   cross.plot <- eval(parse(text=paste("ggplot(data, aes(x=",x,", y=",y,", linetype=",cross,"))",sep="")))
   cross.plot + stat_summary(fun.y=mean, geom="point", position=pos.dodge) +
     stat_summary(fun.data=mean_cl_boot, geom="errorbar", width=0.2, position=pos.dodge) +
@@ -9,5 +8,5 @@ cross.plot <- function (data, x, y, cross, x.lab, y.lab, cross.lab, theme,
     scale_linetype_manual(values=c('solid', 'dashed'), name="Face") +
     theme_bw(base_size=12) + theme +
     theme(legend.position=legend.pos)
-  dev.off()
+  ggsave(output.path, plot=last_plot(), width=output.w, height=output.h)
 }
